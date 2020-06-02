be5cbfc19f653c66edd0fb3a91f0e8359413fc01
Nga Nguyen <ngasdragon@gmail.com>
2020-06-01 22:03:19 -0700

    Added Theme Context to components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/themeContext';
 
 class NavBar extends React.Component {
+  static contextType = ThemeContext
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Context App</h1>
         <ul>
           <li>Home</li>
