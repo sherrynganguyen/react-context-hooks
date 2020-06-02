e99e640d60a9f4e8066bd031c8e7b5e0f13287a7
Nga Nguyen <ngasdragon@gmail.com>
2020-06-01 19:58:57 -0700

    Create component BookList and NavBar

---
+++ src/components/BookList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div className="booklist">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default BookList;
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <h2>Check</h2>
+    </div>
+  );
+}
+
+export default NavBar;
