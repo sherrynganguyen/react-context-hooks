9422dda39eb29803d245e8879c6c8b53ca6cff39
Nga Nguyen <ngasdragon@gmail.com>
2020-06-01 23:13:38 -0700

    rearrange folders

---
+++ react-context/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import BookList from './components/BookList';
+import NavBar from './components/NavBar';
+import ThemeToggle from './components/ThemeToggle';
+
+import ThemeContextProvider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext';
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+      <AuthContextProvider>
+        <NavBar/>
+        <BookList/>
+        <ThemeToggle/>
+      </AuthContextProvider>
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-context/src/contexts/ThemeContext.js
@@ -0,0 +1,31 @@
+import React, { createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends React.Component {
+  state = {
+    isLightTheme: true,
+    light: {
+      syntax: "#555",
+      ui: "#DDD",
+      bg: "#eee"
+    },
+    dark: {
+      syntax: "#DDD",
+      ui: "#333",
+      bg: "#555"
+    },
+  }
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme })
+  }
+  render() {
+    return (
+      <ThemeContext.Provider value ={{...this.state, toggleTheme: this.toggleTheme }}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    )
+  }
+}
+
+export default ThemeContextProvider;
